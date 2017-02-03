56c9936af38ee2db6916f71e606957cdc8607660
Rudra Narayan <rudra1personal@gmail.com>
2017-02-02 23:57:30 -0600

    Add files via upload

---
+++ api/v1/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'sql3117431');
+define('DB_PASSWORD', 'TeEA7WkJfW');
+define('DB_HOST', 'sql3.freemysqlhosting.net');
+define('DB_NAME', 'sql3117431');
+?>
---
+++ api/v1/index.php
@@ -0,0 +1,55 @@
+<?php
+require '.././libs/Slim/Slim.php';
+require_once 'dbHelper.php';
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app = \Slim\Slim::getInstance();
+$db = new dbHelper();
+
+
+// Products
+$app->get('/products', function() { 
+    global $db;
+    $rows = $db->select("products","id,sku,name,description,price,mrp,stock,image,packing,status",array());
+    echoResponse(200, $rows);
+});
+
+$app->post('/products', function() use ($app) { 
+    $data = json_decode($app->request->getBody());
+    $mandatory = array('name');
+    global $db;
+    $rows = $db->insert("products", $data, $mandatory);
+    if($rows["status"]=="success")
+        $rows["message"] = "Product added successfully.";
+    echoResponse(200, $rows);
+});
+
+$app->put('/products/:id', function($id) use ($app) { 
+    $data = json_decode($app->request->getBody());
+    $condition = array('id'=>$id);
+    $mandatory = array();
+    global $db;
+    $rows = $db->update("products", $data, $condition, $mandatory);
+    if($rows["status"]=="success")
+        $rows["message"] = "Product information updated successfully.";
+    echoResponse(200, $rows);
+});
+
+$app->delete('/products/:id', function($id) { 
+    global $db;
+    $rows = $db->delete("products", array('id'=>$id));
+    if($rows["status"]=="success")
+        $rows["message"] = "Product removed successfully.";
+    echoResponse(200, $rows);
+});
+
+function echoResponse($status_code, $response) {
+    global $app;
+    $app->status($status_code);
+    $app->contentType('application/json');
+    echo json_encode($response,JSON_NUMERIC_CHECK);
+}
+
+$app->run();
+?>
